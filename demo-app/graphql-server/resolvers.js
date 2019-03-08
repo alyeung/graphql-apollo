62f0e2eb474a5ee5536ac6b449d1b0bfb1f21364
Allan Yeung <akyeung@gmail.com>
2019-03-07 23:37:53 -0800

    Exercise 1 add cars schema, resolver, and graphQL query

--- demo-app/graphql-server/resolvers.js
+++ demo-app/graphql-server/resolvers.js
@@ -1,7 +1,34 @@
-// import fetch from 'node-fetch';
+// fetch API from web browser using node-fetch
+import fetch from 'node-fetch';
+
 
 export const resolvers = {
   Query: {
-    message: () => 'Hello World!',
+    message:() => 'Hello World!',
+    someNumber: () => '42',
+    customer: () => ({
+      firstName: 'Sally',
+      lastName: 'Thomas',
+      age: 34,
+      height: 63.5,
+    }),
+    //widget: (_1, _2, context) =>{
+      widgets: async(_1, _2, context) =>{
+
+      // promise (async operation) in response rs from server
+      //return fetch(`${context.restURL}/widgets/1`)
+      const res = await fetch(`${context.restURL}/widgets`)
+      
+      // rs converted to json!
+      //.then(res => res.json());
+
+      // promises and generators
+      // not blocking javascript thread
+      return await res.json();
+    },
+      cars: async(_1, _2, context) => {
+        const res = await fetch(`${context.restURL}/cars`)
+        return await res.json();
+      }
   },
 };
--- demo-app/graphql-server/schema.js
+++ demo-app/graphql-server/schema.js
@@ -1,5 +1,34 @@
 export const typeDefs = `
   type Query {
-    message: String
+      message: String,
+      someNumber: Int
+      customer: Person
+      widgets: [Widget]
+      cars: [Car]
+  }
+
+  type Widget {
+    id: Int
+    name: String
+    description: String
+    quantity: Int
+    color: String
+    price: Float
+  }
+
+  type Person {
+    firstName: String
+    lastName: String
+    age: Int
+    height: Float
+  }
+
+  type Car{
+    id: Int
+    make: String
+    model: String
+    year: Int
+    color: String
+    price: Int
   }
 `;
