fd79e67e7d16e1da3eaa48c81f78587d9d3fc793
Allan Yeung <akyeung@gmail.com>
2019-03-09 09:51:19 -0800

    Created WidgetToolQuery.js starter file

---
+++ demo-app/src/queries/widgets/WidgetToolQuery.js
@@ -0,0 +1,29 @@
+import React from 'react'; //react api import
+import {Query} from 'react-apollo';  //Query component that knows how to talk to GraphQL, get data, and make available to react component tree
+import gql from 'graphql-tag';
+
+// copy from graphQL to make sure the query is working first
+// wrap in gql for it to work
+const widgetsQuery = gql`
+    query Widgets{
+        widgets{
+        id
+        name
+        description
+        quantity
+        color
+        price
+        }
+    }
+`;
+
+export const WidgetToolQuery = () => {
+    // jsx, alternative syntax for function calls
+    return <Query query={widgetsQuery}>
+    </Query>;
+
+    // transpiled
+    //return React.createElement(Query, {query: widgetsQuery});
+
+    
+}
