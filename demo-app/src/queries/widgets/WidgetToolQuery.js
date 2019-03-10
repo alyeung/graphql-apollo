439fb332b90fd2f531e48f6b010faf34800882d6
Allan Yeung <akyeung@gmail.com>
2019-03-09 17:59:22 -0800

    widgets are working on localhost, commit it

--- demo-app/src/queries/widgets/WidgetToolQuery.js
+++ demo-app/src/queries/widgets/WidgetToolQuery.js
@@ -1,7 +1,7 @@
 import React from 'react'; //react api import
 import {Query} from 'react-apollo';  //Query component that knows how to talk to GraphQL, get data, and make available to react component tree
 import gql from 'graphql-tag';
-import {WidgetTool} from 
+import {WidgetTool} from '../../components/widgets/WidgetTool';
 
 // copy from graphQL to make sure the query is working first
 // wrap in gql for it to work
@@ -22,12 +22,11 @@ export const WidgetToolQuery = () => {
     // jsx, alternative syntax for function calls
     return <Query query={widgetsQuery}>
     
-    // destructuring
-    {({loading}) => {
+    {( {loading, error, data}) => {
         if(loading) return 'Loading';
         if(error) return 'Error';
 
-        return
+        return <WidgetTool widgets={data.widgets}/>;
     }}
     </Query>;
 
